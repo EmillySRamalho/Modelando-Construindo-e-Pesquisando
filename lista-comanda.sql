86f3b5286acbaa764762c5dcac4d69a4468bb96e
EmillySRamalho <emilly_bsfpb@hotmail.com>
2025-10-22 21:28:59 -0300

    3) Liste todas as comandas (código, data, mesa e nome do cliente) mais uma coluna com o valor total da comanda. Ordene por data esta listagem;

---
+++ lista-comanda.sql
@@ -0,0 +1,19 @@
+SELECT
+    Comanda.codigo AS codigo_comanda,
+    Comanda.data,
+    Comanda.mesa,
+    Comanda.nome_cliente,
+    SUM(ItemComanda.quantidade * Cardapio.preco_unitario) AS valor_total
+FROM
+    Comanda
+JOIN
+    ItemComanda ON Comanda.codigo = ItemComanda.codigo_comanda
+JOIN
+    Cardapio ON ItemComanda.codigo_cardapio = Cardapio.codigo
+GROUP BY
+    Comanda.codigo,
+    Comanda.data,
+    Comanda.mesa,
+    Comanda.nome_cliente
+ORDER BY
+    Comanda.data ASC;
